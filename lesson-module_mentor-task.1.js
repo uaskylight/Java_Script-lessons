bb14ea991a2e7228deadd812c0fb3d937f63734e
AlexT <uaskylight@gmail.com>
2023-01-15 17:31:33 +0200

    hw

--- lesson-module_mentor-task.1.js
+++ lesson-module_mentor-task.1.js
@@ -1,113 +1,143 @@
+// Модуль 1. Заняття 1. Змінні, типи та оператори
 // Example 1 - Математичні оператори
 // Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду.
 
 // const apples = 47;
 // const grapes = 135;
-// const total = apples + grapes;
-// console.log(total);
-// const diff = apples - grapes;
-// console.log(diff);
+// const total = ;
+// console.log(total)
+// const diff = ;
+// console.log(diff)
 
 // Example 2 - Комбіновані оператори
 // Заміни вираз перевизначення комбінованим оператором +=.
 
 // let students = 100;
-// students += 50; // students = students + 50
-// // students -= 10;
+// students = students + 50;
 // console.log(students);
 
 // Example 3 - Пріоритет операторів
-// Розбери пріоритет операторів в інструкції привласнення значення константи result.
+// Розбери пріоритет операторів в інструкції привласнення значення змінної result.
 
 // const result = 108 + 223 - 2 * 5;
 // console.log(result);
 
-// let num = 4;
-// console.log(num ** 0.5)
-// console.log(num)
 
 // Example 4 - Клас Math
-// Напиши скрипт, який виводить у консоль заокруглені вгору/вниз і т.д. значення константи value. Використовуй методи Math.floor(), Math.ceil() та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+// Напиши скрипт, який виводить у консоль заокруглені вгору / вниз і т.д.значення змінної value. 
+// Використовуй методи Math.floor(), Math.ceil() та Math.round().Перевір що буде в консолі при значеннях 27.3 та 27.9.
 
-// const value = 27.9;
-// console.log(Math.floor(value))
-// console.log(Math.ceil(value))
-// console.log(Math.round(value))
+// const value = 27.5;
+
 
 // Example 5 - Шаблонні рядки
 // Склади фразу за допомогою шаблонних рядків A has B bots in stock, де A, B - змінні вставлені в рядок.
 
-// const companyName = "Cyberdyne Systems";
+// const companyName = 'Cyberdyne Systems';
 // const repairBots = 150;
 // const defenceBots = 50;
-// const message = `${companyName} has ${repairBots + defenceBots} bots in stock`;
+// const message = ``;
 // console.log(message); // "Cyberdyne Systems has 200 bots in stock"
+
 
 // Example 6 - Методи рядків та чейнінг
 // Напиши скрипт, який розраховує індекс маси тіла людини. Для цього необхідно розділити вагу в кілограмах на квадрат висоти людини у метрах.
 
-// Вага та висота зберігаються у змінних weight та height, але не як числа, а в вигляді рядків (спеціально для завдання). Не цілі числа можуть бути задані у вигляді 24.7 або 24,7, тобто як роздільник дробової частини може бути кома.
+// Вага та висота зберігаються у змінних weight та height, але не як числа, а в вигляді рядків(спеціально для завдання). 
+// Не цілі числа можуть бути задані у вигляді 24.7 або 24, 7, тобто як роздільник дробової частини може бути кома.
 
 // Індекс маси тіла необхідно округлити до однієї цифри після коми;
 
-// let weight = "88,3";
-// let height = "1.75";
 
-// variant 1
+// let weight = '88,3';
+// let height = '1.75';
 
-// weight = Number(weight.replace(",", "."));
-// height = Number(height.replace(",", "."));
-
-// const bmi = +(weight / Math.pow(height, 2)).toFixed(1);
+// const bmi = ;
 // console.log(bmi); // 28.8
 
-// variant 2
-
-// 1. Знайти індекс коми
-// 2. Скопіюємо число після коми
-// 3. Cлеїмо цілу частину з дробовою і поставио правильний розділювач (.)
-
-// const commaIndex = weight.indexOf(",");
-// const valueAfterComma = weight.slice(commaIndex + 1);
-// const valueBeforeComma = Number.parseInt(weight);
-
-// weight = +`${valueBeforeComma}.${valueAfterComma}`;
-
-// const bmi = +(weight / Math.pow(height, 2)).toFixed(1);
-// console.log(bmi); // 28.8
 
 // Example 7 - Оператори порівняння та приведення типів
 // Яким буде результат виразів?
 
-// console.log(5 > 4); //true
+// console.log(5 > 4);
 
-// console.log(10 >= '7'); //true
+// console.log(10 >= '7');
 
-// console.log('2' > '12'); // true
-// '2' > '1' -> 50 > 49
-// console.log("1234".charCodeAt(0))
+// console.log('2' > '12');
 
-// console.log('2' < '12'); // false
+// console.log('2' < '12');
 
-// console.log('4' == 4); // true
+// console.log('4' == 4);
 
-// console.log('6' === 6); // false
+// console.log('6' === 6);
 
-// console.log('false' === false); // false
+// console.log('false' === false);
 
-// console.log(1 == true); // true
+// console.log(1 == true);
 
-// console.log(1 === true); // false
+// console.log(1 === true);
 
-// console.log('0' == false); // true
+// console.log('0' == false);
 
-// console.log('0' === false); // false
+// console.log('0' === false);
 
-// console.log('Papaya' < 'papaya'); // true
+// console.log('Papaya' < 'papaya');
 
-// console.log('Papaya' === 'papaya'); // false
+// console.log('Papaya' === 'papaya');
 
-// console.log(undefined == null); // true
+// console.log(undefined == null);
 
-// console.log(undefined === null); // false
+// console.log(undefined === null);
 
+
+// Example 8 - Логічні оператори
+// Яким буде результат виразів?
+
+// console.log(true && 3);
+
+// console.log(false && 3);
+
+// console.log(true && 4 && 'kiwi');
+
+// console.log(true && 0 && 'kiwi');
+
+// console.log(true || 3);
+
+// console.log(true || 3 || 4);
+
+// console.log(true || false || 7);
+
+// console.log(null || 2 || undefined);
+
+// console.log((1 && null && 2) > 0);
+
+// console.log(null || (2 && 3) || 4);
+
+
+// Example 9 - Значення за замовчуванням та оператор нульового злиття
+// Отрефактори код так, щоб у змінну value присвоювалося значення змінної incomingValue, якщо воно не рівне undefined або null. 
+// В іншому випадку має присвоюватися значення defaultValue.Перевір роботу скрипта для наступних значень змінної incomingValue: null, undefined, 0, false.
+// Використовуй оператор ?? (nullish coalescing operator).
+
+// const incomingValue = 5;
+// const defaultValue = 10;
+// const value = incomingValue || defaultValue;
+// console.log(value);
+
+
+// Example 10 - Оператор % та методи рядків
+// Напиши скрипт, який переведе значення totalMinutes (кількість хвилин) в рядок у форматі годин та хвилин HH:MM.
+
+// 70 покаже 01:10
+// 450 покаже 07:30
+// 1441 покаже 24:01
+// const totalMinutes = 70;
+
+// const hours = Math.floor(totalMinutes / 60);
+// const minutes = totalMinutes % 60;
+// console.log(hours);
+// console.log(minutes);
+
+// const doubleDigitHours = String(hours).padStart(2, 0);
+// const doubleDigitMinutes = String(minutes).padStart(2, 0);
+// console.log(`${doubleDigitHours}:${doubleDigitMinutes}`);
